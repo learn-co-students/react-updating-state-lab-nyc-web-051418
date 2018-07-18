52535b71960fcb00439beb8a4b1707cfa534ebdd
squamuglia <maxsmouha@gmail.com>
2018-07-18 09:41:27 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  upTimeOneClickThanks = () => {
+    let newTime = this.state.timesClicked + 1;
+    this.setState(
+      {
+        timesClicked: newTime
+      },
+      console.log(newTime)
+    );
+  };
+  render() {
+    return (
+      <button onClick={this.upTimeOneClickThanks}>
+        {this.state.timesClicked}
+      </button>
+    );
+  }
+}
+
+export default DigitalClicker;
